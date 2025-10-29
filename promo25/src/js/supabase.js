bd0fb3b6d5605990187f1b10e5df13462836950e
odoi21 <hudsonlucas862@gmail.com>
2025-10-29 18:40:26 -0300

    .env

--- promo25/src/js/supabase.js
+++ promo25/src/js/supabase.js
@@ -1,6 +1,6 @@
-// supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ujlbhnskejfyznknyymb.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqbGJobnNrZWpmeXpua255eW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNTUxMDQsImV4cCI6MjA3NjczMTEwNH0.w_-caFIVD1qoPIgaHcX_Ci1LIGvxoLdKyUxGST2DEH8'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
